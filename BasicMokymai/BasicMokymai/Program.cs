14dcaaf0519b817c681a14ed6d46b657ff473c7e
jkersys <justinaskersys@gmail.com>
2022-06-06 20:51:45 +0300

    save

--- BasicMokymai/BasicMokymai/Program.cs
+++ BasicMokymai/BasicMokymai/Program.cs
@@ -11,6 +11,12 @@
             Console.Write("eilutėje");
             Console.WriteLine();
             Console.WriteLine("tekstas kitoje eilutėje");
+            Console.Write("tekstas");
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("išvedimas" + "vienoje" + "eilutėje"); //konkatnacija
+            Console.WriteLine("{0} {1} {2}", "išvedimas", "vienoje", "eilutėje"); // kompozicija
+            Console.WriteLine($"{"išvedimas"} {"vienoje"} {"eilutėje"}"); // interpoliacija
         }
     }
 }
