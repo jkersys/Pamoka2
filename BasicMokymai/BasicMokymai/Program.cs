584b19f5cd942c442f168075a0cbb54394ebb008
jkersys <justinaskersys@gmail.com>
2022-06-07 18:27:09 +0300

    save

--- BasicMokymai/BasicMokymai/Program.cs
+++ BasicMokymai/BasicMokymai/Program.cs
@@ -6,17 +6,60 @@
         {
             Console.WriteLine("Hello, World!");
 
-            Console.Write("išvedimas");
-            Console.Write("vienoje ");
-            Console.Write("eilutėje");
-            Console.WriteLine();
-            Console.WriteLine("tekstas kitoje eilutėje");
-            Console.Write("tekstas");
+            //Console.Write("išvedimas");
+            //Console.Write("vienoje ");
+            //Console.Write("eilutėje");
+            //Console.WriteLine();
+            // Console.WriteLine("tekstas kitoje eilutėje");
+            // Console.Write("tekstas");
 
-            Console.WriteLine("---------------");
-            Console.WriteLine("išvedimas" + "vienoje" + "eilutėje"); //konkatnacija
-            Console.WriteLine("{0} {1} {2}", "išvedimas", "vienoje", "eilutėje"); // kompozicija
-            Console.WriteLine($"{"išvedimas"} {"vienoje"} {"eilutėje"}"); // interpoliacija
+            //Console.WriteLine("---------------");
+            //Console.WriteLine("išvedimas" + "vienoje" + "eilutėje"); //konkatnacija
+            // Console.WriteLine("{0} {1} {2}", "išvedimas", "vienoje", "eilutėje"); // kompozicija
+            //Console.WriteLine($"{"išvedimas"} {"vienoje"} {"eilutėje"}"); // interpoliacija
+            // Console.WriteLine("---------------");
+
+            //Console.WriteLine("----press any key to continue---");
+            //Console.ReadKey();
+            // Console.Clear();
+
+            //Console.WriteLine("įveskite savo vadą, o aš jį pakartosiu");
+
+            //Console.WriteLine("Įveskite raidę");
+            //var key = Console.ReadKey();
+            // Console.WriteLine("įvestassimbolis {0}", key.keychar);
+
+            console.write("justinas");
+            console.writeline("-----press any key to continue----------");
+            console.readkey();
+            console.clear();
+
+            console.writeline("įveskite savo vardą, o aš jį pakartosiu:");
+            console.writeline("o štai mano pakartojimas " + console.readline());
+            console.writeline("o štai mano pakartojimas {0}", console.readline());
+            console.writeline($"o štai mano pakartojimas {console.readline()}");
+
+            console.writeline("įveskite savo vardą, o aš jį pakartosiu:");
+            console.writeline("o štai mano pakartojimas " + console.readline());
+            console.writeline("o štai mano pakartojimas {0}", console.readline());
+            console.writeline($"o štai mano pakartojimas {console.readline()}");
+
+            console.writeline("įveskite raidę");
+            var key = console.readkey();
+            console.writeline("ivestas simbolis {0}", key.keychar);
+            console.writeline("ivestas simbolis {0}", key.key);
+            console.writeline("ivestas simbolis {0}", (int)key.keychar);
+
+            console.writeline("ivestas simbolis {0}", console.readkey().keychar);
+            console.writeline("ivestas simbolis {0}", (int)console.readkey().keychar);
+
+
+
+            Console.ReadKey();
+
+
+
+
         }
     }
 }
---
+++ BasicMokymai/komentarai/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("***komentarai***");
+Console.WriteLine("kokie būna komentarai");
+// čia paprastas komentaras
+/*
+ * čia
+ * bulti-line
+ * komentarai
+ * */
+Console.WriteLine();//komentaras
+
+var kintamasis = /*kažkoks komentaras*/ "galima taip";
+
+Console.WriteLine("kodėl reikia rašyti komentarus?");
+
+Console.WriteLine("kaip rašyti gerus komentarus?");
+/*
+ *1. Reikia rašyti komentarus, kuris yra funkciškai išbgaigtas
+ *2. Rašyti komentarus, kurie prideda prasmės, o ne pasako ką kodas daro
+ */
+//Blogas komentaro pvz:
+//Console.writeLine naudomajas į ekraną išvesti Hello World!
+Console.WriteLine("hello world!");
+
+
+//Geras komentaras pvz:
+//Išvesti Hello World! tai mano pirmoji veikianti programa
+Console.WriteLine("hello world!");
