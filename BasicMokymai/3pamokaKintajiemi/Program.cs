7ee8badac71b061a1c26364525440b0f55cf1f72
jkersys <justinaskersys@gmail.com>
2022-06-08 22:02:19 +0300

    kintamieji

--- BasicMokymai/3pamokaKintajiemi/Program.cs
+++ BasicMokymai/3pamokaKintajiemi/Program.cs
@@ -46,6 +46,46 @@
 
 
 
+            /*PARAŠYTI PROGRAMĄ KURIOJE SAUGOME :
+    • MOKYKLOS PAVADINIMĄ
+    • KURSO PAVADINIMĄ
+    • STUDENTŲ SKAIČIŲ
+    • ŠIANDIENOS DATĄ
+    • VISUS KINTAMUOSIUS IŠVESTI Į EKRANĄ
+            */
+
+            var pavadinimas = "Codeacademy";
+            var kursopavadinimas = "CA.NET";
+            var studentuSkaicius = 19;
+            var siandienosdata = new DateTime(2022,06,08);
+
+            Console.WriteLine($"\n{pavadinimas}\n{kursopavadinimas}\n{studentuSkaicius}\n{siandienosdata}");
+
+            /*PAPILDYTI PROGRAMĄ IR PRIDĖTI:
+• KURSO PRADŽIOS DATĄ
+• KURSO PABAIGOS DATĄ
+• Sužinoti skirtumą tarp pradžios ir dabartinės datos(dienomis)
+• VISUS KINTAMUOSIUS IŠVESTI Į EKRANĄ
+            */
+
+            var pradziosdata = new DateTime(2022, 05, 30);
+            var pabaigosdata = new DateTime(2022, 12, 30);
+            TimeSpan dienuskirtumas = siandienosdata - pradziosdata;
+
+
+            Console.WriteLine($"\n{pavadinimas}\n{kursopavadinimas}\n{studentuSkaicius}\n{siandienosdata}\n{pradziosdata}\n{pabaigosdata}\n{dienuskirtumas.Days}");
+
+            /*Sukurkite tris kintamuosius.tekstinio, sveiko skaitmens ir loginio tipo.
+Parašykite programą kuri į konsolę visus aprašytus kintamuosius vienoje eilutėje atskiriant tarpu
+            */
+
+            //galima naudoti var vietoj strink int bool
+            string tekstinis = "tekstas";
+            int sveikasSkaicius = 2;
+            bool loginis = true;
+
+            Console.WriteLine($"{tekstinis} {sveikasSkaicius} {loginis}");
+
 
 
         }
