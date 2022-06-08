71d0e25e229995b74d8ce0d65ac24e5aec94c9d0
jkersys <justinaskersys@gmail.com>
2022-06-08 19:06:17 +0300

    Baigtas

--- BasicMokymai/Tower of hanoi/Program.cs
+++ BasicMokymai/Tower of hanoi/Program.cs
@@ -2,7 +2,7 @@
 //Console.WriteLine("Hello, World!");
 
 
-//prisiskiriam kintamuosius, sulygiuojam
+//prisiskiriam kintamuosius, sulygiuojam (\u0023 reiškia #)
 string stulp1_eil1 = "      |";
 string stulp2_eil1 = "            |"; 
 string stulp3_eil1 = "            |";
@@ -33,10 +33,14 @@ Console.WriteLine("      ----1sulp-------2stulp-------3stulp----");
 
 Console.WriteLine("\n\n\n------tęsti-------\n");
 
-//pasidarom kad po paspaudomo atidarytų kita veiksmą
+//pasidarom kad po paspaudomo atidarytų kitą veiksmą
 Console.ReadKey();
 
-//apverčiam kintamuosius, palygiuojam
+
+
+Console.WriteLine("\n 2. Apverskite pirmą sulpelį ir išveskite visą Tower of Hanoi\n");
+
+//apverčiam piramdę su kintamaisiais ir palygiuojam, buvo galima redaguot tik pirmą stulp, bet kol mokaus ir pirmą kartą gyvenime koduoju gerai ir taip, jeigu ateityje naudosiu kaip pavizdį gerai pasigalvot ties kiekvienu veiksmu...
 stulp1_eil1 = " \u0023\u0023\u0023\u0023\u0023|\u0023\u0023\u0023\u0023\u0023";
 stulp1_eil2 = "   \u0023\u0023\u0023|\u0023\u0023\u0023";
 stulp1_eil3 = "    \u0023\u0023|\u0023\u0023";
@@ -53,9 +57,6 @@ stulp3_eil3 = "            |";
 stulp3_eil4 = "            |";
 stulp3_eil5 = "            |";
 
-Console.WriteLine("\n 2. Apverskite pirmą sulpelį ir išveskite visą Tower of Hanoi\n");
-
-
 Console.WriteLine($"1eil. {stulp1_eil1}{stulp2_eil1}{stulp3_eil1}");
 Console.WriteLine($"2eil. {stulp1_eil2}{stulp2_eil2}{stulp3_eil2}");
 Console.WriteLine($"3eil. {stulp1_eil3}{stulp2_eil3}{stulp3_eil3}");
@@ -65,13 +66,13 @@ Console.WriteLine("      ----1sulp-------2stulp-------3stulp----");
 
 Console.WriteLine("\n\n\n------tęsti-------\n");
 
-//pasidarom kad po paspaudomo atidarytų kita veiksmą
+//pasidarom kad po paspaudomo atidarytų kitą veiksmą
 Console.ReadKey();
 
 Console.WriteLine("\n 3. Išvalykite pirmą stulpelį ir išveskite tuščią Tower of Hanoi\n");
 
 
-//vėl prisiskiriam kintamuosius
+//vėl prisiskiriam kintamuosius ir lygiuojam(būtų užtekę prisiskirt tik pirmam stulpeliui)
 stulp1_eil1 = "      |";
 stulp1_eil2 = "      |";
 stulp1_eil3 = "      |";
@@ -94,14 +95,14 @@ Console.WriteLine("      ----1sulp-------2stulp-------3stulp----");
 
 Console.WriteLine("\n\n\n------tęsti-------\n");
 
-//pasidarom kad po paspaudomo atidarytų kita veiksmą
+//pasidarom kad po paspaudomo atidarytų kitą veiksmą
 Console.ReadKey();
 
 
 Console.WriteLine("\n 4.	Į kiekvieno stulpelio 5eil įdėkite po 4 dalių elementą ir išveskite Tower of Hanoi\n");
 
 
-//keičiam kintamuosius
+//keičiam kintamuosius, pasilygiuojam
 stulp1_eil5 = " \u0023\u0023\u0023\u0023\u0023|\u0023\u0023\u0023\u0023\u0023";
 stulp2_eil5 = "  \u0023\u0023\u0023\u0023\u0023|\u0023\u0023\u0023\u0023\u0023";
 stulp3_eil5 = "  \u0023\u0023\u0023\u0023\u0023|\u0023\u0023\u0023\u0023\u0023";
@@ -115,13 +116,13 @@ Console.WriteLine("      ----1sulp-------2stulp-------3stulp----");
 
 Console.WriteLine("\n\n\n------tęsti-------\n");
 
-//pasidarom kad po paspaudomo atidarytų kita veiksmą
+//pasidarom kad po paspaudomo atidarytų kitą veiksmą
 Console.ReadKey();
 
 
 Console.WriteLine("\n 5. Į 1stulp 5eil įdėkite 4 dalių elementą, 2sutup 5eil - 3 dalių, 3sutup 4eil - 1 dalies, 3sutup 5eil - 2 dalių, ir išveskite Tower of Hanoi\n");
 
-//Vėl keičiu kintamuosiu ir sulygiuoju
+//Vėl keičiu kintamuosius ir sulygiuoju
 stulp1_eil5 = " \u0023\u0023\u0023\u0023\u0023I\u0023\u0023\u0023\u0023\u0023";
 stulp2_eil5 = "    \u0023\u0023\u0023|\u0023\u0023\u0023";
 stulp3_eil4 = "           \u0023|\u0023";
@@ -137,12 +138,12 @@ Console.WriteLine("      ----1sulp-------2stulp-------3stulp----");
 
 Console.WriteLine("\n\n\n------tęsti-------\n");
 
-//pasidarom kad po paspaudomo atidarytų kita veiksmą
+//pasidarom kad po paspaudomo atidarytų kitą veiksmą
 Console.ReadKey();
 
 Console.WriteLine("\n 6.	Į 1stulp 4eil įdėkite tokį pat elementą kaip yra 3stup 4eil, ir išveskite Tower of Hanoi\n");
 
-// kintamieji ir lygiavimas
+//kintamųjų reikšmių keitimas ir lygiavimas
 stulp1_eil5 = " \u0023\u0023\u0023\u0023\u0023|\u0023\u0023\u0023\u0023\u0023";
 stulp2_eil5 = "    \u0023\u0023\u0023|\u0023\u0023\u0023";
 stulp3_eil4 = "           \u0023|\u0023";
@@ -181,12 +182,12 @@ Console.WriteLine("      ----1sulp-------2stulp-------3stulp----");
 
 Console.WriteLine("\n\n\n------tęsti-------\n");
 
-//pasidarom kad po paspaudomo atidarytų kita veiksmą
+//pasidarom kad po paspaudomo atidarytų kitą veiksmą
 Console.ReadKey();
 
 Console.WriteLine("\n 8. Į 3stulp sudėkite teisingą piramidę. 1stulp ir 2 stulp turi likti tušti, išveskite Tower of Hanoi\n");
 
-//pakeičiam kintamuosius,bet turbūt būtų užtekę tik pakeisti 3 stulpelio.
+//pakeičiam kintamuosius(būtų užtekę tik dalį pakeist).
 stulp1_eil1 =  "      |";
 stulp1_eil2 =  "      |";
 stulp1_eil3 =  "      |";
@@ -253,5 +254,5 @@ Console.WriteLine($"4eil. {stulp1_eil4}{stulp2_eil4}{stulp3_eil4}");
 Console.WriteLine($"5eil. {stulp1_eil5}{stulp2_eil5}{stulp3_eil5}");
 Console.WriteLine("      ----1sulp-------2stulp-------3stulp----");
 
-//Visumoj kodas atrodo baisiai ir neaišku ar teisingas, bet lyg ir veikia
+//Visumoj kodas atrodo baisiai ir neaišku ar teisingas, bet lyg ir veikia.
 
